6accf21ef3397a33053687314d9e8e00eb20856b
Huevos <rowland@moralet.com>
2020-05-04 00:13:06 +0200

    Add lunix34k

---
+++ boxbranding/src/machines/lunix34k.py
@@ -0,0 +1,86 @@
+# branding file for lunix34k
+
+def getBrandOEM():
+	return "dags"
+
+def getDisplayType():
+	return "textlcd"
+
+def getHaveAVJACK():
+	return "False"
+
+def getHaveCI():
+	return "True"
+
+def getHaveDVI():
+	return "False"
+
+def getHaveHDMI():
+	return "True"
+
+def getHaveHDMIinFHD():
+	return "False"
+
+def getHaveHDMIinHD():
+	return "False"
+
+def getHaveMiniTV():
+	return "False"
+
+def getHaveRCA():
+	return "True"
+
+def getHaveSCART():
+	return "False"
+
+def getHaveSCARTYUV():
+	return "False"
+
+def getHaveTranscoding1():
+	return "transcoding"
+
+def getHaveWOL():
+	return "False"
+
+def getHaveWWOL():
+	return "False"
+
+def getHaveYUV():
+	return "False"
+
+def getImageArch():
+	return "cortexa15hf-neon-vfpv4"
+
+def getImageFileSystem():
+	return "tar.bz2"
+
+def getImageFolder():
+	return "update/lunix3-4k"
+
+def getMachineBrand():
+	return "qviart"
+
+def getMachineBuild():
+	return "lunix34k"
+
+def getMachineKernelFile():
+	return "oe_kernel.bin"
+
+def getMachineMKUBIFS():
+	return ""
+
+def getMachineMake():
+	return "lunix34k"
+
+def getMachineMtdKernel():
+	return "mmcblk0p1"
+
+def getMachineMtdRoot():
+	return "mmcblk0p4"
+
+def getMachineRootFile():
+	return "rootfs.tar.bz2"
+
+def getMachineUBINIZE():
+	return ""
+

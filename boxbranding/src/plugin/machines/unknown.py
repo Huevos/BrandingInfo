b873e9dca2742bf11b0235245614841ddc094713
Huevos <rowland@moralet.com>
2020-05-16 11:36:57 +0200

    [unknown.py:] this is just a template of a possible machine

---
+++ boxbranding/src/plugin/machines/unknown.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+# branding file for unknown. This file is not used.
+
+def getBoxType():
+	return ""
+
+def getMachineName():
+	return ""
+
+def getBrandOEM():
+	return ""
+
+def getDisplayType():
+	return ""
+
+def getHaveAVJACK():
+	return ""
+
+def getHaveCI():
+	return ""
+
+def getHaveDVI():
+	return ""
+
+def getHaveHDMI():
+	return "True"
+
+def getHaveHDMIinFHD():
+	return ""
+
+def getHaveHDMIinHD():
+	return ""
+
+def getHaveMiniTV():
+	return ""
+
+def getHaveRCA():
+	return ""
+
+def getHaveSCART():
+	return ""
+
+def getHaveSCARTYUV():
+	return ""
+
+def getHaveTranscoding1():
+	return ""
+
+def getHaveWOL():
+	return ""
+
+def getHaveWWOL():
+	return ""
+
+def getHaveYUV():
+	return ""
+
+def getImageArch():
+	return ""
+
+def getImageFileSystem():
+	return ""
+
+def getImageFolder():
+	return ""
+
+def getMachineBrand():
+	return ""
+
+def getMachineBuild():
+	return ""
+
+def getMachineKernelFile():
+	return ""
+
+def getMachineMKUBIFS():
+	return ""
+
+def getMachineMake():
+	return ""
+
+def getMachineMtdKernel():
+	return ""
+
+def getMachineMtdRoot():
+	return ""
+
+def getMachineProcModel():
+	return ""
+
+def getMachineRootFile():
+	return ""
+
+def getMachineUBINIZE():
+	return ""
+
